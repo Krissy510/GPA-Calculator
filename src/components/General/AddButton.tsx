076a9d2893b4be3535ec9f7aa297271bbb662760
Kris Raviphas <2827932s@student.gla.ac.uk>
2023-08-17 17:23:17 +0100

    Add add/delete buttons

---
+++ src/components/General/AddButton.tsx
@@ -0,0 +1,29 @@
+import AddIcon from "@mui/icons-material/Add";
+import {IconButton} from "@mui/material";
+import {IButtonProps} from "../../Interfaces.ts";
+
+const AddButton = ({disabled, onClick}: IButtonProps) => {
+    return (
+        <IconButton
+            aria-label={'add'}
+            sx={{
+                width: '50px',
+                height: '50px',
+                backgroundColor: 'primary.main',
+                color: 'white',
+                '&:hover': {
+                    backgroundColor: 'primary.dark',
+                },
+                '&:disabled': {
+                    backgroundColor: 'grey',
+                },
+            }}
+            disabled={disabled}
+            onClick={onClick}
+        >
+            <AddIcon/>
+        </IconButton>
+    );
+};
+
+export default AddButton;
---
+++ src/components/General/DeleteButton.tsx
@@ -0,0 +1,29 @@
+import {IconButton} from "@mui/material";
+import {IButtonProps} from "../../Interfaces.ts";
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const DeleteButton = ({disabled, onClick}: IButtonProps) => {
+    return (
+        <IconButton
+            aria-label={'delete'}
+            sx={{
+                width: '50px',
+                height: '50px',
+                backgroundColor: '#ff1744',
+                color: 'white',
+                '&:hover': {
+                    backgroundColor: '#b2102f',
+                },
+                '&:disabled': {
+                    backgroundColor: 'grey',
+                },
+            }}
+            disabled={disabled}
+            onClick={onClick}
+        >
+            <DeleteIcon/>
+        </IconButton>
+    );
+};
+
+export default DeleteButton;
